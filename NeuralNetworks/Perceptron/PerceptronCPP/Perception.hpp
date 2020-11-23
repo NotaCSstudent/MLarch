13e776f1ad16c5585ced55b8d9e1078afeb27e0a
mahamud babu <mahmudbabu94@gmail.com>
2020-11-23 14:48:39 -0500

    added some functionality

--- NeuralNetworks/Perceptron/PerceptronCPP/Perception.hpp
+++ NeuralNetworks/Perceptron/PerceptronCPP/Perception.hpp
@@ -1,15 +1,22 @@
 #ifndef PERCEPTION_
 #define PERCEPTION_
-
+#include <vector>
 class net
 {
-private:
-    int n;
 public:
-    net(int x);
+    net(float time,int period);
+    float netinput(vector<float> x);
+    int prediction (vector<float> x);
+    voit fit(vector<vector<float>> x , vector<float> y);
+private:
+    
+    float eta;
+    int periods;
+    vector <float> m_w;
     
     
-}
+    
+};
 
 
 
